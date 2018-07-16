5c641eec6a0befd59920ee874178ff33ff4ab66f
d-das <jdawsonweehunt@gmail.com>
2018-07-16 12:08:58 -0500

    Changes to be committed: 	deleted:    fixpulsebug.sh 	modified:   load_layouts.sh 	modified:   rofi-search
     Untracked files:
    	client_secrets.json
    	fixpulsebug
    	gbackup
    	listUsers.sh
    	notes.AppImage
    	notes.sh
    	quickstart.py
    	rofi-translate
    	scrot-2
    	spotify_block.sh
    	useKVM.sh
    	wiki-search
    	wolfram-search

--- load_layouts.sh
+++ load_layouts.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+# run `i3-save-tree --workspace N > ~/.config/i3/workspace_N.json`
+# then edit the file to match template of others
 i3-msg "workspace 1; append_layout ~/.config/i3/workspace_1.json"
-(atom &);
+(atom --name editor &);
 
 i3-msg "workspace 2; append_layout ~/.config/i3/workspace_2.json"
-(i3-sensible-terminal &)
-(google-chrome &);
+(i3-sensible-terminal --name launch &)
+(google-chrome --name inspect &);
 
 i3-msg "workspace 3; append_layout ~/.config/i3/workspace_3.json"
-(firefox &);
+(firefox --name work &);
 
-#i3-msg "workspace 9; append_layout ~/.config/i3/workspace_9.json"
-#(firefox &);
+i3-msg "workspace 4; append_layout ~/.config/i3/workspace_4.json"
+(slack --name talk &);
+
+i3-msg "workspace 8; append_layout ~/.config/i3/workspace_8.json"
+(standard-notes --name notes &);
 
 i3-msg "workspace 10; append_layout ~/.config/i3/workspace_10.json"
-(spotify &);
+(spotify --name music &);
