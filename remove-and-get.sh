75f96d5eb79a58be0d334feeb0092edfcaff82c6
d-das <jdawsonweehunt@gmail.com>
2018-07-16 12:26:36 -0500

    added i3 config

---
+++ remove-and-get.sh
@@ -0,0 +1,17 @@
+sudo apt remove account-plugin-aim account-plugin-facebook account-plugin-flickr account-plugin-jabber account-plugin-salut account-plugin-twitter account-plugin-windows-live account-plugin-yahoo aisleriot brltty colord deja-dup deja-dup-backend-gvfs duplicity empathy empathy-common evolution-data-server-online-accounts example-content gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects gnomine landscape-common libreoffice-avmedia-backend-gstreamer libreoffice-base-core libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-gnome libreoffice-gtk libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-presentation-minimizer libreoffice-style-galaxy libreoffice-style-human libreoffice-writer libsane libsane-common mcp-account-manager-uoa python3-uno rhythmbox rhythmbox-plugins rhythmbox-plugin-zeitgeist sane-utils shotwell shotwell-common telepathy-gabble telepathy-haze telepathy-idle telepathy-indicator telepathy-logger telepathy-mission-control-5 telepathy-salut thunderbird thunderbird-gnome-support totem totem-common totem-plugins unity-scope-audacious unity-scope-chromiumbookmarks unity-scope-clementine unity-scope-colourlovers unity-scope-devhelp unity-scope-firefoxbookmarks unity-scope-gdrive unity-scope-gmusicbrowser unity-scope-gourmet unity-scope-manpages unity-scope-musicstores unity-scope-musique unity-scope-openclipart unity-scope-texdoc unity-scope-tomboy unity-scope-video-remote unity-scope-virtualbox unity-scope-yelp unity-scope-zotero unity-webapps-common;
+
+sudo apt update;
+sudo apt upgrade;
+
+sudo apt install vim feh i3 compton pathogen git python-pip freeglut3 freeglut3-dev nodejs npm tor torbrowser-launcher htop node-typescript libreoffice-writer ebook-convert android-tools-adb android-tools-fastboot android-sdk android-sdk-build-tools android-sdk-common android-sdk-platform-tools android-sdk-platform-tools-common default-jdk gradle aircrack-ng wireshark cryptcat; 
+
+sudo npm install -g ionic;
+sudo npm install -g cordova;
+sudo npm install -g lite-server;
+
+#https://developer.android.com/studio/index.html
+#install android-sdk
+#amend .bashrc
+#https://cordova.apache.org/docs/en/latest/guide/platforms/android/
+#export ANDROID_HOME=/home/dbac/Android/Sdk/
+#export PATH=${PATH}:/home/dbac/Android/Sdk/platform-tools:/home/dbac/Android/Sdk/tools
